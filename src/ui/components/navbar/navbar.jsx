67560ef262a0fcbaf5927e003bd97c764448b679
Melissa Gómez <gisellmego2001@gmail.com>
2022-11-24 00:28:02 -0500

    Primeros cambios Navbar

--- src/ui/components/navbar/navbar.jsx
+++ src/ui/components/navbar/navbar.jsx
@@ -3,10 +3,27 @@ import { CartWidget } from "./cart-widget"
 export const Navbar = () => {
     return (
         <>
-            <ul className="hola">
-                <li><a href="/">Home</a></li>
-                <li><a href="/ejemplo">Products</a></li>
+    <header>
+        <nav className="navigation">
+            <ul className="navigation-bar">
+                <li className="navigation-bar-item">
+                    <a href="./index.html">Inicio</a>
+                </li>
+                <li className="navigation-bar-item">
+                    <a href="./sections/servicios.html">Marcas Zapatos</a>
+                </li>
+                <li className="navigation-bar-item">
+                    <a href="./sections/experiencia.html">Marcas Ropa</a>
+                </li>
+                <li className="navigation-bar-item">
+                    <a href="./sections/portafolio.html">Lo Más vendido</a>
+                </li>
+                <li className="navigation-bar-item">
+                    <a href="./sections/contacto.html">Contacto</a>
+                </li>
             </ul>
+        </nav>
+    </header>
             <CartWidget />
         </>
     )
